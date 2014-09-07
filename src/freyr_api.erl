6a072d3cd42732e067cc0c727355ff4806b9be67
Claudio Ortolina <claudio@new-bamboo.co.uk>
2014-09-07 19:12:37 +0100

    Adds hourly endpoint.

--- src/freyr_api.erl
+++ src/freyr_api.erl
@@ -11,6 +11,12 @@ init(_Route, _Req, State) ->
 get("/readings", _Req, State) ->
   Readings = freyr_storage:all(),
   Serialized = freyr_reading_serializer:serialize(Readings),
+  {ok, {json, Serialized}, State};
+
+get("/readings/hourly/:hour", Req, State) ->
+  Hour = leptus_req:param(Req, hour),
+  Readings = freyr_storage:by_hour(binary_to_integer(Hour)),
+  Serialized = freyr_reading_serializer:serialize(Readings),
   {ok, {json, Serialized}, State}.
 
 terminate(_Reason, _Route, _Req, _State) ->
