c571bf3a9824e890f711021e0558092405c35aec
Claudio Ortolina <claudio@new-bamboo.co.uk>
2014-09-07 08:38:58 +0100

    Adds Ruby tester.

---
+++ tester.rb
@@ -0,0 +1,13 @@
+require 'socket'
+
+temperature = [21.2, 22.5, 20.8, 19.5, 24.2, 23.7, 21.7]
+brightness  = [978,  850,  400,  390,  750,  650,  722]
+moisture    = [3500, 4000, 2200, 3700, 2800, 3421, 2738]
+
+TCPSocket.open 'localhost', 5678 do |s|
+  10.times do |n|
+    msg = "temperature:f:#{temperature.sample}|brightness:i:#{brightness.sample}|moisture:i:#{moisture.sample}"
+    s.send msg, 0
+    sleep 0.3
+  end
+end
