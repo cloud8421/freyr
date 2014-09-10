217e3dbe30a81c217d7334543db8734589e4369c
Claudio Ortolina <claudio@new-bamboo.co.uk>
2014-09-10 09:14:27 +0100

    Adds freyr_device model.

---
+++ src/freyr_device.erl
@@ -0,0 +1,20 @@
+-module(freyr_device).
+
+-export([find/1]).
+
+-include("freyr_reading.hrl").
+
+find(DeviceId) ->
+  Device = freyr_device_store:by_id(DeviceId),
+  Metadata = metadata(DeviceId),
+  {ok, Averages} = maps:find(averages, Metadata),
+  {ok, LastReading} = maps:find(last_reading, Metadata),
+  #{device => Device,
+    averages => Averages,
+    last_reading => LastReading}.
+
+metadata(DeviceId) when is_list(DeviceId) ->
+  Readings = [LastReading | _Rest] = freyr_reading_store:by_device(DeviceId),
+  Averages = freyr_reading_calc:averages(Readings),
+  #{averages => Averages,
+    last_reading => LastReading}.
---
+++ test/freyr_device_test.erl
@@ -0,0 +1,47 @@
+-module(freyr_device_test).
+
+-c(freyr_device).
+-c(mocks).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-include("../src/freyr_reading.hrl").
+-include("../src/freyr_device.hrl").
+
+find_test() ->
+  meck:new(freyr_reading_store),
+  meck:expect(freyr_reading_store, by_device, fun("abc4rq1q3rw") -> readings() end),
+  meck:new(freyr_device_store),
+  meck:expect(freyr_device_store, by_id, fun("abc4rq1q3rw") -> device() end),
+  ExpectedAverages = #{average_temperature=>24.0,
+                       average_brightness=>4000,
+                       average_moisture=>500},
+  [Reading | _Rest] = readings(),
+  Expected = #{device => device(),
+               last_reading => Reading,
+               averages => ExpectedAverages},
+  ?assertEqual(freyr_device:find("abc4rq1q3rw"), Expected).
+
+readings() ->
+  [#freyr_reading{
+      uuid=mocks:default_uuid(),
+      device_id="abc4rq1q3rw",
+      temperature=25.0,
+      brightness=3000,
+      moisture=400,
+      timestamp=mocks:default_timestamp()},
+   #freyr_reading{
+      uuid=mocks:default_uuid(),
+      device_id="abc4rq1q3rw",
+      temperature=23.0,
+      brightness=5000,
+      moisture=600,
+      timestamp=mocks:default_timestamp()}].
+
+device() ->
+  #freyr_device{
+     uuid="abc1234xyz",
+     name="The first",
+     location="Living Room",
+     created_at=mocks:default_timestamp(),
+     updated_at=mocks:default_timestamp()}.
